4fa044845288150ef731bb59edf5ace7cbc0c3fd
Maxim Pavelchuk <pavelchukmaks2204@gmail.com>
2024-04-14 08:50:16 +0300

    modified:   frontend/src/components/BookItem.jsx 	modified:   frontend/src/pages/AddRent.jsx 	modified:   frontend/src/pages/BooksPage.jsx 	modified:   frontend/src/styles/BookItem.css 	modified:   frontend/src/styles/BooksPage.css 	modified:   frontend/src/styles/addRent.css 	modified:   frontend/src/styles/register.css 	modified:   node_modules/.package-lock.json

--- frontend/src/pages/BooksPage.jsx
+++ frontend/src/pages/BooksPage.jsx
@@ -77,7 +77,7 @@ const BooksPage = () => {
   };
 
   return (
-    <div className="max-w-900">
+    <div className='books-main'>
       <div className="flex justify-between gap-4">
         <div className="flex text-2xl text-white flex-col gap-5 basis-4/5">
           <div className="search-container mb-4">
@@ -89,7 +89,7 @@ const BooksPage = () => {
               value={searchTerm}
             />
           </div>
-          <div className="books-list" style={{ maxHeight: 'calc(100vh - 200px)', overflowY: 'auto' }}>
+          <div className="books-list" style={{ display:'flex',flexDirection:'row',flexWrap:"wrap", maxHeight: 'calc(100vh - 200px)', overflowY: 'auto' }}>
             {filteredBooks.length > 0 ? (
               filteredBooks.map((book, idx) => (
                 <BookItem key={idx} book={book} />
