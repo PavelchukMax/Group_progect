c8e8bbebc7729fa72c36e3bd8a3393172959401e
BUILTIN\Administrators <dxxdimaxxd228@gmail.com>
2024-04-23 21:47:44 +0300

    done

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import "../styles/mainPage.css";
-
+import library1 from "../images/library1.jpg"
+import library2 from "../images/library2.jpg"
+import library3 from "../images/library3.jpg"
+import library4 from "../images/library4.jpg"
+import library5 from "../images/library5.jpg"
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 /**
  * Головна сторінка програми.
  * @module MainPage
@@ -13,6 +19,25 @@ const MainPage = () => {
   const { user } = useSelector((state) => state.auth);
   return (
     <div className="main-page-container">
+        <div style={{margin:"auto", marginBottom:0}}>
+        <Carousel width={600} height={300} interval={2000} showThumbs={false} showStatus={false} showIndicators={false} autoPlay={true} infiniteLoop={true}>
+                <div width={600} height={300}>
+                    <img width={600} height={300} src={library1} />
+                </div>
+                <div width={600} height={300}>
+                    <img width={600} height={300} src={library2} />
+                </div>
+                <div width={600} height={300}>
+                    <img width={600} height={300} src={library3} />
+                </div>
+                <div width={600} height={300}>
+                    <img width={600} height={300} src={library4} />
+                </div>
+                <div width={600} height={300}>
+                    <img width={600} height={300} src={library5} />
+                </div> 
+            </Carousel>
+            </div> 
       <div className='main-page-text'>
         Ласкаво просимо, <div className='bold main-page-text'>{user.username}!</div>
         </div>
